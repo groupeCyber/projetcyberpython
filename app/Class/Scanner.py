d517f2276a45247a2dfc22ebbffa275073b1cefa
Karkowin <karkowin@gmail.com>
2023-11-27 17:00:51 +0100

    Implementig scanner logic and logging

--- app/Class/Scanner.py
+++ app/Class/Scanner.py
@@ -1,11 +1,40 @@
-from scapy import *
+from scapy.all import *
+import time
 
 class Scanner:
     def __init__(self, interface):
         self.interface = interface
-        self.packets = []
 
     def scan(self):
         while True:
-            self.packets.append(sniff(iface=self.interface, count=1))
-            yield self.packets
+            packets = (sniff(iface=self.interface, count=10))
+            self.logging(packets)
+            yield packets
+
+    def logging(self, packets):
+        for packet in packets:
+            log_entry = self.processing_packet(packet)
+            with open("logs/general_logs.txt", "a") as log_file:
+                log_file.write(log_entry)
+    
+    def processing_packet(self, packet):
+        timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        src_ip, dst_ip = None, None
+        if IP in packet:
+            src_ip = packet[IP].src
+            dst_ip = packet[IP].dst
+        src_port, dst_port, proto = None, None, None
+        if TCP in packet:
+            print(packet[TCP])
+            src_port = packet[TCP].sport
+            dst_port = packet[TCP].dport
+            proto = "TCP"
+        elif UDP in packet:
+            print(packet[UDP])
+            src_port = packet[UDP].sport
+            dst_port = packet[UDP].dport
+            proto = "UDP"
+        elif ICMP in packet:
+            proto = "ICMP"
+            src_port, dst_port = "-", "-"
+        return f"{timestamp} | Protocol: {proto} | Source IP: {src_ip} | Destination IP: {dst_ip} | Source Port: {src_port} | Destination Port: {dst_port}\n"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,7 @@
-test ;
+from Class.Scanner import Scanner
+
+if __name__ == '__main__':
+    scanner = Scanner('eth0')
+    scan  = scanner.scan()
+    for packet in scan:
+            print(packet)
